5f6e168983bb5b109fa5218d8c2e1f0fec516fa7
Bowo <wibowosulistio@gmail.com>
2022-10-22 20:17:06 +0700

    Improve inline comments

--- debug-log-manager.php
+++ debug-log-manager.php
@@ -20,10 +20,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 define( 'DLM_VERSION', '1.8.6' );
 define( 'DLM_SLUG', 'debug-log-manager' );
-define( 'DLM_URL', plugins_url( '/', __FILE__ ) );
-define( 'DLM_PATH', plugin_dir_path( __FILE__ ) );
-// define( 'DLM_BASE', plugin_basename( __FILE__ ) );
-// define( 'DLM_FILE', __FILE__ );
+define( 'DLM_URL', plugins_url( '/', __FILE__ ) ); // e.g. https://www.example.com/wp-content/plugins/this-plugin/
+define( 'DLM_PATH', plugin_dir_path( __FILE__ ) ); // e.g. /home/user/apps/wp-root/wp-content/plugins/this-plugin/
+// define( 'DLM_BASE', plugin_basename( __FILE__ ) ); // e.g. plugin-slug/this-file.php
+// define( 'DLM_FILE', __FILE__ ); // /home/user/apps/wp-root/wp-content/plugins/this-plugin/this-file.php
 
 // Register autoloading classes
 spl_autoload_register( 'dlm_autoloader' );
@@ -38,14 +38,29 @@ function dlm_autoloader( $class_name ) {
 
     $namespace = 'DLM';
 
+    // Only process classes within this plugin's namespace
+
     if ( false !== strpos( $class_name, $namespace ) ) {
 
         // Assemble file path for the class
-        $path = str_replace( $namespace, "", $class_name ); // \DLM\Classes\The_Name => \Classes\The_Name
-        $path = str_replace( "\\", DIRECTORY_SEPARATOR, strtolower( $path ) ); // => /classes/the_name
-        $path = str_replace( "_", "-" , $path ) . '.php'; // => /classes/the-name.php
-        $path = str_replace( "classes" . DIRECTORY_SEPARATOR, "classes" . DIRECTORY_SEPARATOR . "class-", $path ); // => /classes/class-the-name.php
-        $path = DLM_PATH . $path; // => /plugin-path/classes/class-the-name.php
+
+        // \DLM\Classes\The_Name => \Classes\The_Name
+        $path = str_replace( $namespace, "", $class_name );
+
+        // \Classes\The_Name => /classes/the_name
+        $path = str_replace( "\\", DIRECTORY_SEPARATOR, strtolower( $path ) );
+
+        // /classes/the_name =>  /classes/the-name.php
+        $path = str_replace( "_", "-", $path ) . '.php';
+
+        // /classes/the-name.php => /classes/class-the-name.php
+        $path = str_replace( "classes" . DIRECTORY_SEPARATOR, "classes" . DIRECTORY_SEPARATOR . "class-", $path );
+
+        // Remove first '/'
+        $path = substr( $path, 1 );
+
+        // Get /plugin-path/classes/class-the-name.php
+        $path = DLM_PATH . $path;
 
         if ( file_exists( $path ) ) {
             require_once( $path );
